e7c442356d4dcf5d6caa1acbf51f933bd7f8681e
Aaron Gonzalez <aaronraulgonzalez@gmail.com>
2022-05-04 13:53:11 -0500

    Prettify

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -5,9 +5,14 @@ const NavBar = () => {
           <li>
             <div className="logo"></div>
           </li>
-          <li>
-            <a href="/">Home</a>
-          </li>
+          <li><a href="/">Home </a></li>
+          <li><a href="/">TV Shows </a></li>
+          <li><a href="/">Movies </a></li>
+          <li><a href="/">New & Popular </a></li>
+          <li><a href="/">My List </a></li>
+          <li><a href="/">Browse by Language </a></li>
+          <li><a href="/">Kids </a></li>
+          <li><a href="/">DVD </a></li>
         </ul>
       </div>
     )
